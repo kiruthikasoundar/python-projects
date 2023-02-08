1192371efc53456918a1934a08d92995a73cc7e5
kiruthikasoundar <36938852+kiruthikasoundar@users.noreply.github.com>
2023-02-08 14:48:14 -0500

    Add files via upload
    added complete code for blackjack game

--- main.py
+++ main.py
@@ -1,92 +1,126 @@
-from game_data import data
+############### Blackjack Project #####################
+
+#Difficulty Normal 😎: Use all Hints below to complete the project.
+#Difficulty Hard 🤔: Use only Hints 1, 2, 3 to complete the project.
+#Difficulty Extra Hard 😭: Only use Hints 1 & 2 to complete the project.
+#Difficulty Expert 🤯: Only use Hint 1 to complete the project.
+
+############### Our Blackjack House Rules #####################
+
+## The deck is unlimited in size. 
+## There are no jokers. 
+## The Jack/Queen/King all count as 10.
+## The the Ace can count as 11 or 1.
+## Use the following list as the deck of cards:
+## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+## The cards in the list have equal probability of being drawn.
+## Cards are not removed from the deck as they are drawn.
+
+##################### Hints #####################
+
+#Hint 1: Go to this website and try out the Blackjack game: 
+#   https://games.washingtonpost.com/games/blackjack/
+#Then try out the completed Blackjack project here: 
+#   http://blackjack-final.appbrewery.repl.run
+
+#Hint 2: Read this breakdown of program requirements: 
+#   http://listmoz.com/view/6h34DJpvJBFVRlZfJvxF
+#Then try to create your own flowchart for the program.
+
+#Hint 3: Download and read this flow chart I've created: 
+#   https://drive.google.com/uc?export=download&id=1rDkiHCrhaf9eX7u7yjM1qwSuyEk-rPnt
+
+#Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
+#11 is the Ace.
 import random
-from art import logo, vs
-# from replit import clear
+from replit import clear
+from art import logo
 
-def get_random_account():
-  """Get data from random account"""
-  return random.choice(data)
+def deal_card():
+  """Returns a random card from the deck."""
+  cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+  card = random.choice(cards)
+  return card
 
-def format_data(account):
-  """Format account into printable format: name, description and country"""
-  name = account["name"]
-  description = account["description"]
-  country = account["country"]
-  # print(f'{name}: {account["follower_count"]}')
-  return f"{name}, a {description}, from {country}"
+#Hint 6: Create a function called calculate_score() that takes a List of cards as input 
+#and returns the score. 
+#Look up the sum() function to help you do this.
+def calculate_score(cards):
+  """Take a list of cards and return the score calculated from the cards"""
 
-def check_answer(guess, a_followers, b_followers):
-  """Checks followers against user's guess 
-  and returns True if they got it right.
-  Or False if they got it wrong.""" 
-  if a_followers > b_followers:
-    return guess == "a"
+  #Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
+  if sum(cards) == 21 and len(cards) == 2:
+    return 0
+  #Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
+  if 11 in cards and sum(cards) > 21:
+    cards.remove(11)
+    cards.append(1)
+  return sum(cards)
+
+#Hint 13: Create a function called compare() and pass in the user_score and computer_score. If the computer and user both have the same score, then it's a draw. If the computer has a blackjack (0), then the user loses. If the user has a blackjack (0), then the user wins. If the user_score is over 21, then the user loses. If the computer_score is over 21, then the computer loses. If none of the above, then the player with the highest score wins.
+def compare(user_score, computer_score):
+  #Bug fix. If you and the computer are both over, you lose.
+  if user_score > 21 and computer_score > 21:
+    return "You went over. You lose 😤"
+
+
+  if user_score == computer_score:
+    return "Draw 🙃"
+  elif computer_score == 0:
+    return "Lose, opponent has Blackjack 😱"
+  elif user_score == 0:
+    return "Win with a Blackjack 😎"
+  elif user_score > 21:
+    return "You went over. You lose 😭"
+  elif computer_score > 21:
+    return "Opponent went over. You win 😁"
+  elif user_score > computer_score:
+    return "You win 😃"
   else:
-    return guess == "b"
+    return "You lose 😤"
 
+def play_game():
 
-def game():
   print(logo)
-  score = 0
-  game_should_continue = True
-  account_a = get_random_account()
-  account_b = get_random_account()
 
-  while game_should_continue:
-    account_a = account_b
-    account_b = get_random_account()
+  #Hint 5: Deal the user and computer 2 cards each using deal_card()
+  user_cards = []
+  computer_cards = []
+  is_game_over = False
 
-    while account_a == account_b:
-      account_b = get_random_account()
+  for _ in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
 
-    print(f"Compare A: {format_data(account_a)}.")
-    print(vs)
-    print(f"Against B: {format_data(account_b)}.")
-    
-    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
-    a_follower_count = account_a["follower_count"]
-    b_follower_count = account_b["follower_count"]
-    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+  #Hint 11: The score will need to be rechecked with every new card drawn and the checks in Hint 9 need to be repeated until the game ends.
 
-    clear()
-    print(logo)
-    if is_correct:
-      score += 1
-      print(f"You're right! Current score: {score}.")
+  while not is_game_over:
+    #Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
+    user_score = calculate_score(user_cards)
+    computer_score = calculate_score(computer_cards)
+    print(f"   Your cards: {user_cards}, current score: {user_score}")
+    print(f"   Computer's first card: {computer_cards[0]}")
+
+    if user_score == 0 or computer_score == 0 or user_score > 21:
+      is_game_over = True
     else:
-      game_should_continue = False
-      print(f"Sorry, that's wrong. Final score: {score}")
+      #Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
+      user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
+      if user_should_deal == "y":
+        user_cards.append(deal_card())
+      else:
+        is_game_over = True
 
-game()
+  #Hint 12: Once the user is done, it's time to let the computer play. The computer should keep drawing cards as long as it has a score less than 17.
+  while computer_score != 0 and computer_score < 17:
+    computer_cards.append(deal_card())
+    computer_score = calculate_score(computer_cards)
 
-'''
+  print(f"   Your final hand: {user_cards}, final score: {user_score}")
+  print(f"   Computer's final hand: {computer_cards}, final score: {computer_score}")
+  print(compare(user_score, computer_score))
 
-FAQ: Why does choice B always become choice A in every round, even when A had more followers? 
-
-Suppose you just started the game and you are comparing the followers of A - Instagram (364k) to B - Selena Gomez (174k). Instagram has more followers, so choice A is correct. However, the subsequent comparison should be between Selena Gomez (the new A) and someone else. The reason is that everything in our list has fewer followers than Instagram. If we were to keep Instagram as part of the comparison (as choice A) then Instagram would stay there for the rest of the game. This would be quite boring. By swapping choice B for A each round, we avoid a situation where the number of followers of choice A keeps going up over the course of the game. Hope that makes sense :-)
-
-'''
-
-
-
-# Generate a random account from the game data.
-
-# Format account data into printable format.
-
-# Ask user for a guess.
-
-# Check if user is correct.
-## Get follower count.
-## If Statement
-
-# Feedback.
-
-# Score Keeping.
-
-# Make game repeatable.
-
-# Make B become the next A.
-
-# Add art.
-
-# Clear screen between rounds.
+#Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
+while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
+  clear()
+  play_game()
